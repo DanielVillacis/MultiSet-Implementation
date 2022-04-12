768a91b50a35095a126e06d367753d2e4135344e
Daniel Villacis <daniel1997v@gmail.com>
2022-04-11 21:50:11 -0400

    avancer et reculer implantes mais a verifier

--- Devoir-6-MultiSet/unordered_set2.h
+++ Devoir-6-MultiSet/unordered_set2.h
@@ -11,7 +11,6 @@
 
 /////////////////////////////////////////////////////////////////////
 // rehash
-
 template <typename TYPE,typename classe_de_dispersion>
 void unordered_multiset<TYPE, classe_de_dispersion>::rehash(size_t nbalv){
 }
@@ -19,20 +18,36 @@ void unordered_multiset<TYPE, classe_de_dispersion>::rehash(size_t nbalv){
 
 ///////////////////////////////////////////
 // avancer et reculer un iterateur
-
 template <typename TYPE,typename classe_de_dispersion>
 void unordered_multiset<TYPE,classe_de_dispersion>::iterator::avancer(){
     //plante si on essaie d'avancer apres la fin
+    if(POS != (*ALV)->end()){
+        POS++;
+    }
+    else {
+        while((*ALV) == nullptr){
+            ALV++;
+        }
+    }
+    POS = (*ALV)->begin();
 }
 
 template <typename TYPE,typename classe_de_dispersion>
 void unordered_multiset<TYPE,classe_de_dispersion>::iterator::reculer(){
     //plante si on essaie de reculer avant le debut
+    if(POS != (*ALV)->begin()){
+        POS--;
+    }
+    else {
+        while((*ALV) == nullptr){
+            ALV--;
+        }
+        POS = (*ALV)->begin();
+    }
 }
 
 /////////////////////////////////////////////////////////////////////
 // fonctions generatrices
-
 template <typename TYPE,typename classe_de_dispersion>
 typename unordered_multiset<TYPE,classe_de_dispersion>::iterator
 unordered_multiset<TYPE,classe_de_dispersion>::insert(const TYPE& VAL){
@@ -51,8 +66,10 @@ template <typename TYPE,typename classe_de_dispersion>
 typename unordered_multiset<TYPE,classe_de_dispersion>::iterator
 unordered_multiset<TYPE,classe_de_dispersion>::erase
 (typename unordered_multiset<TYPE,classe_de_dispersion>::iterator i){
-    iterator retour;
-    return retour;
+//    iterator retour;
+//    return retour;
+    (*i.ALV)->erase(i.POS);
+    return i;
 }
 
 /////////////////////////////////////////////////////////////////////
