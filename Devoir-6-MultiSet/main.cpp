76f6c3de40998bd8479d7b741dd9a7401e51aeae
Daniel Villacis <daniel1997v@gmail.com>
2022-04-15 11:08:03 -0400

    main du prof

--- Devoir-6-MultiSet/main.cpp
+++ Devoir-6-MultiSet/main.cpp
@@ -1,14 +1,67 @@
 //
 //  main.cpp
-//  Devoir-6-MultiSet
+//  unordered_multiset
 //
-//  Created by Daniel Villacis on 2022-04-11.
+//  Jean Goulet 2022-03-23
 //
 
 #include <iostream>
+#include <iomanip>
+#include "unordered_set.h"
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    
+    unordered_multiset<double> UM;
+    cout<<"Un ensemble vide"<<endl;
+    UM.afficher();
+    cout<<endl<<"Construire un petit ensemble de base"<<endl;
+    for(double d=55;d<100;d+=12)UM.insert(d);
+    for(double d=37;d<100;d+=9)UM.insert(d);
+    UM.afficher();
+    
+    cout<<endl<<"Enlever 91"<<endl;
+    UM.erase(91);
+    UM.afficher();
+    
+    cout<<endl<<"Enlever le premier"<<endl;
+    auto it=UM.erase(UM.begin());
+    cout<<"Le suivant est "<<*it<<endl;
+    UM.afficher();
+ 
+    size_t nb=UM.count(55);
+    cout<<"Il y a "<<nb<<" 55 dans l'ensemble"<<endl;
+    cout<<"enlever le premier"<<endl;
+    it=UM.find(55);
+    UM.erase(it);
+    UM.afficher();
+    
+    cout<<endl<<"Itérer sur l'ensemble"<<endl;
+    for(it=UM.begin();it!=UM.end();++it)cout<<*it<<" ";
+    cout<<endl;
+    cout<<endl<<"Itérer à reculons"<<endl;
+    for(it=UM.end();it!=UM.begin();)cout<<*--it<<" ";
+    cout<<endl;
+
+    cout<<endl<<"Enlever les 5 premiers"<<endl;
+    UM.erase(UM.begin());
+    UM.erase(UM.begin());
+    UM.erase(UM.begin());
+    UM.erase(UM.begin());
+    UM.erase(UM.begin());
+    UM.afficher();
+
+ 
+    cout<<endl<<"Grossir l'ensemble"<<endl;
+    for(double d=1;d<100;d+=3)UM.insert(d);
+    for(double d=7;d<100;d+=6)UM.insert(d);
+    for(double d=37;d<100;d+=9)UM.insert(d);
+    for(double d=55;d<100;d+=12)UM.insert(d);
+
+    UM.afficher();
+
+    cout << "Fin du programme"<<endl;
     return 0;
 }
+
